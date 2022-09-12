928ee1b853d56095224b6bb2275839c94e0d4d62
PabloNValdez <pablovaldez0301@gmail.com>
2022-09-12 12:55:53 -0300

    Componentes generales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NotFound404Component } from './shared/not-found404/not-found404.component';
+import { MaintenanceComponent } from './shared/maintenance/maintenance.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NotFound404Component,
+    MaintenanceComponent
   ],
   imports: [
     BrowserModule,
